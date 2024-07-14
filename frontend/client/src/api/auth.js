c195a13be62254161049611ffe208dc43d1cd6fd
ikavyathota <146860492+ikavyathota@users.noreply.github.com>
2024-07-15 00:43:30 +0530

    Update auth.js

--- frontend/client/src/api/auth.js
+++ frontend/client/src/api/auth.js
@@ -1,1 +1,21 @@
+import axios from 'axios';
+
+const API_URL = 'http://localhost:8000/api/users'; 
+export const registerUser = async (email, password) => {
+    try {
+        const response = await axios.post(`${API_URL}/register`, { email, password });
+        return response; 
+    } catch (error) {
+        throw error; 
+    }
+};
+
+export const loginUser = async (email, password) => {
+    try {
+        const response = await axios.post(`${API_URL}/login`, { email, password });
+        return response;
+    } catch (error) {
+        throw error; 
+    }
+};
 
