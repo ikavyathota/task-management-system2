e89e5e1b3885dbc500c577ad484c481b022f2fbe
ikavyathota <146860492+ikavyathota@users.noreply.github.com>
2024-07-15 07:19:11 +0530

    Update authMiddleware.js

--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -1,1 +1,30 @@
 
+const jwt = require('jsonwebtoken');
+
+const authenticate = (req, res, next) => {
+    const { authorization } = req.headers;
+
+    if (!authorization || !authorization.startsWith('Bearer ')) {
+        return res.status(401).json({ error: 'Token not provided' });
+    }
+
+    const token = authorization.split(' ')[1];
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = decoded;
+        next();
+    } catch (error) {
+        if (error.name === 'TokenExpiredError') {
+            return res.status(401).json({ error: 'Token expired' });
+        } else if (error.name === 'JsonWebTokenError') {
+            return res.status(401).json({ error: 'Invalid token' });
+        } else {
+            return res.status(500).json({ error: 'Internal server error' });
+        }
+    }
+};
+
+module.exports = authenticate;
+
+    
