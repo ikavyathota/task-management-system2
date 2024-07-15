ccd522157681e1d458825178d339cae05cd02a5e
ikavyathota <146860492+ikavyathota@users.noreply.github.com>
2024-07-15 07:19:51 +0530

    Update connect.js

--- backend/connect.js
+++ backend/connect.js
@@ -1,1 +1,10 @@
 
+
+const mysql = require('mysql2/promise');
+const mysqlPool  = mysql.createPool({
+    host:'localhost',
+    user:'root',
+    password:'admin',
+    database:'studentdb'
+})
+module.exports=mysqlPool
