e9ffed160c69a97d40b3b6678b2e43184b556d88
ikavyathota <146860492+ikavyathota@users.noreply.github.com>
2024-07-15 07:20:33 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,28 @@
 
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+const userRoutes = require('./routes/userRoutes');
+const taskRoutes = require('./routes/taskRoutes');
+
+app.use(express.json());
+
+// Routes
+const corsOptions = {
+  origin: 'http://localhost:3000', 
+  optionsSuccessStatus: 200 
+};
+
+app.use(cors(corsOptions));
+app.use('/api/users', userRoutes);
+app.use('/api/tasks', taskRoutes);
+
+app.get('/', (req, res) => {
+  res.send('Hello, world!');
+});
+
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
